e0c5b71718a4e03d0a5b4197d8298866273a7a85
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2024-12-13 18:27:59 +0000

    Leaving code for today was able to get it to create a random pattern have to now add in user input and compare if it matches

--- Simon/script.js
+++ Simon/script.js
@@ -1,9 +1,10 @@
 
 const gridItems = document.querySelectorAll("#gameGrid .grid-Item");
 const gameGrid = document.getElementById("gameGrid");
+
 function asignColour (){
-    const colours = ["red","yellow","green","blue"];
-    gridItems.forEach((item, i) => {
+    const colours = ["red","lime","blue","yellow","orangered","fuchsia","aqua","white","maroon","green","navy","olive","purple","teal","gray","crimson","mediumseagreen","dodgerblue","gold","tomato","darkorchid","darkturquoise","firebrick","turquoise","black"];
+    gridButtons.forEach((item, i) => {
         console.log(colours[i]);
         item.style.backgroundColor = `${(colours[i])}`;
     });
@@ -11,28 +12,38 @@ function asignColour (){
 let currentSize = 2;
 let gridButtons = Array.from(gridItems);
 function gridSizeControler(){
-    gameGrid.style.gridTemplateColumns = `repeat(${currentSize+ 1}, 1fr)`;
-    gameGrid.style.gridTemplateRows = `repeat(${currentSize+ 1}, 1fr)`;
-    ++currentSize;
-    console.log(currentSize);
-    
-    function newButton(){
-        gridSize = currentSize * currentSize;
-        console.log(gridSize);
-        while((gridButtons.length) != gridSize){
-            const button = document.createElement("button");
-            gameGrid.appendChild(button);
-            button.classList.add("grid-Item");
-            gridButtons.push(button);
-        }
+    if (currentSize < 5){
+        gameGrid.style.gridTemplateColumns = `repeat(${currentSize+ 1}, 1fr)`;
+        gameGrid.style.gridTemplateRows = `repeat(${currentSize+ 1}, 1fr)`;
+        ++currentSize;
+        console.log(currentSize);
         
-        gridButtons.forEach((item,index) => {
-            console.log(`Grid item${index + 1}: ${item.textContent}`);
-            item.style.backgroundColor = `lightcoral`;
-        })
-    }
-    newButton();
+        function newButton(){
+            gridSize = currentSize * currentSize;
+            console.log(gridSize);
+                while((gridButtons.length) != gridSize){
+                    const button = document.createElement("button");
+                    gameGrid.appendChild(button);
+                    button.classList.add("grid-Item");
+                    gridButtons.push(button);
+                    asignColour();
+                }
+            }
+            newButton();
+        }
 }
+    
+let sequence = []
+let sequenceNum = 1;
 function gameControler(){
+    let nextNum = Math.floor(Math.random() * gridButtons.length + 1);
+    console.log(nextNum);
+    if((sequence.length) < sequenceNum){
+        sequence.push(nextNum);
+        console.log(sequence);
+        ++sequenceNum;
+        console.log(`sequence num ${sequenceNum}`);
+    }
 }
-gameGrid.onclick = gridSizeControler;
+gameGrid.onclick = gameControler;
+asignColour();
